ac167daf9b41f05f39ee2eb7ebb3ecff0e668916
Your Namenhung1410 <gnuhn1410@gmail.com>
2019-10-11 15:09:19 +0700

    first commit

---
+++ AccessToken/src/main/java/com/anttech/AccessToken/App.java
@@ -0,0 +1,13 @@
+package com.anttech.AccessToken;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
